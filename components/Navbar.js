732e3b856b051f5341db5eac81a6f658433a9633
Cadu <72165203+caduveloso@users.noreply.github.com>
2024-07-14 15:20:04 -0300

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
 
             <div className='w-full flex-none md:flex justify-between'>
 
-                <div className='flex flex-col lg:flex-row w-full justify-start gap-2 lg:gap-12 items-center ml-3'> {/*Left area*/}
+                <div className='flex flex-col lg:flex-row w-full justify-center lg:justify-start gap-1 lg:gap-12 items-center ml-0 lg:ml-3'> {/*Left area*/}
 
                     <div className='flex items-baseline mb-1'>
                         <button>
@@ -17,9 +17,9 @@ export default function Navbar() {
                             </Link>
                         </button> 
                     </div>
-                    <span className='text-white w-full tracking-wider leading-none pb-1 lg:pl-12 lg:border-l border-white items-center lg:items-start flex flex-col'>
-                        <span className='font-bold text-lg lg:text-2xl text-center lg:text-left'>Graphic Design & web Development</span>
-                        <span className='font-thin text-xs lg:text-sm leading-none mt-2 lg:mt-0'>Curated Showcase of my Latest Work</span>
+                    <span className='text-white w-full tracking-wider leading-none pb-1 pl-0 lg:pl-12 lg:border-l border-white items-center lg:items-start flex flex-col'>
+                        <span className='font-normal lg:font-bold text-xl lg:text-2xl text-center lg:text-left'>Graphic Design & web Development</span>
+                        <span className='font-thin text-xs lg:text-sm leading-none mt-2 lg:mt-0 hidden lg:block'>Curated Showcase of my Latest Work</span>
                     </span>
                     {/* <span className='block lg:hidden items-center gap-[2px] text-white text-xs w-full tracking-wider leading-4 pb-1 pl-5 border-l border-white'>
                         <span className='font-bold'>design</span> <br/> <span className='font-bold'>code</span>

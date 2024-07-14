15b6832c7bb8633c69ebe32ebf2f2616b2ccbdeb
Cadu <72165203+caduveloso@users.noreply.github.com>
2024-07-14 14:47:35 -0300

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,25 +4,27 @@ import Link from 'next/link';
 export default function Navbar() {
     return (
 
-        <div className='sticky items-center top-0 flex py-3 lg:py-4 pl-4 pr-6 lg:h-24 bg-black bg-opacity-90 z-10'>
+        <div className='sticky items-center top-0 flex py-6 lg:py-4 pl-4 pr-6 lg:h-24 bg-black bg-opacity-90 z-10'>
 
             <div className='w-full flex-none md:flex justify-between'>
 
-                <div className='flex w-full justify-start gap-6 items-center'> {/*Left area*/}
+                <div className='flex w-full justify-start gap-12 items-center ml-3'> {/*Left area*/}
 
-                    <div className='flex items-baseline'>
+                    <div className='flex items-baseline mb-1'>
                         <button>
                             <Link href='/'>
-                            <span className='text-4xl lg:text-4xl text-white font-bold uppercase'>Cadu</span><span className='ml-1 text-4xl lg:text-4xl text-white font-thin uppercase'>Veloso</span>
+                                <span className='text-2xl lg:text-2xl text-white font-bold'>Cadu</span><span className='ml-[2px] text-2xl lg:text-2xl text-white font-thin'>Veloso</span>
                             </Link>
                         </button> 
                     </div>
-                    <span className='text-white text-xs w-full tracking-wider leading-4 pb-1 pl-5 border-l border-white hidden lg:block'>
-                        <span>Curated Showcase of my Latest Work as a Graphic Designer, Motion Artist and Front-End Developer:</span> <br/> <span className='font-bold'>A Valuable Fusion of Design and Development Expertise</span>
+                    <span className='text-white w-full tracking-wider leading-6 pb-1 pl-12 border-l border-white block'>
+                        <span className='font-bold text-xl lg:text-2xl'>Graphic Design & web Development</span>
+                        <br/>
+                        <span className='font-thin text-xs lg:text-sm'>Curated Showcase of my Latest Work</span>
                     </span>
-                    <span className='text-white text-xs w-full tracking-wider leading-4 pb-1 pl-5 border-l border-white block lg:hidden'>
-                        <span className='font-bold'>design</span> <br/> <span className='font-bold'>code</span><br/> <span className='font-bold'>motion</span>
-                    </span>
+                    {/* <span className='block lg:hidden items-center gap-[2px] text-white text-xs w-full tracking-wider leading-4 pb-1 pl-5 border-l border-white'>
+                        <span className='font-bold'>design</span> <br/> <span className='font-bold'>code</span>
+                    </span> */}
 
                 </div>
 
